bb661d6fdb32b0aaf573ecda757f6de37419f214
Shazaf02 <70133900@student.uol.edu.pk>
2023-10-09 15:40:53 -0400

    done

---
+++ 9-10-2023/q6.py
@@ -0,0 +1,3 @@
+name =input("enter the name ")
+number = len(name)*3
+print ("Hello {}, your lucky number is {}".format(name, number))
---
+++ 9-10-2023/q8.py
@@ -0,0 +1,3 @@
+name ="Manny"
+number = len(name)*3
+print ("Hello {number}, your lucky number is {name}".format(name=name, number=number))
