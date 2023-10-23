0b0f6707473e19e04358405d793ce73144a49397
Shazaf02 <70133900@student.uol.edu.pk>
2023-10-23 15:52:34 -0400

    work

---
+++ 23-10-2023/q10.py
@@ -0,0 +1,17 @@
+item = [1,2,3,4,5]
+try:
+   item = item[6]
+except Exception as e:
+       print(e, "cant")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 23-10-2023/q9.py
@@ -0,0 +1,17 @@
+item = [1,2,3,4,5]
+try:
+   item = item[6]
+except:   
+       print(item)
+
+
+
+
+
+
+
+
+
+
+
+
